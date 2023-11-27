78fa977b6c19425ef760e4bf72121c7ad8d2dc03
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-11-27 20:59:53 -0300

    Rework everything

---
+++ main/go/controller.go
@@ -0,0 +1,30 @@
+components {
+  id: "main"
+  component: "/main/scripts/controller.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "menu"
+  component: "/main/gui/menu.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
